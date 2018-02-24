c049f778b05693ce8dcae2e2d227cf968564f900
Developer <Developer@DESKTOP-S9V5D1L>
2018-02-25 00:23:27 +0100

    First commit.

---
+++ CartesianProduct2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartesianProduct2
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sets> AllSets = new List<Sets>();
+
+            Console.WriteLine(" How many sets do you want to have? ");
+            string numberOfSets = Console.ReadLine();
+            int NumberOfSets = Convert.ToInt32(numberOfSets);
+
+            for (int j = 0; j< NumberOfSets; j++)
+            {
+                Sets set = new Sets();
+
+                Console.WriteLine(" How many numbers do you want to put in set {0}? ", j);
+                string numbersTotal = Console.ReadLine();
+                int TotalNumbers = Convert.ToInt32(numbersTotal);
+
+                set.SetElementsToTheSet(TotalNumbers);
+
+                AllSets.Add(set);
+            }
+            List<List<int>> tmpList = new List<List<int>>();
+
+            Sets CartesianProductionTemp = new Sets();
+            CartesianProductionTemp = AllSets[0];
+            List<Sets> CartesianProduction = new List<Sets>();
+           
+            for ( int l=1; l< AllSets.Count; l+=1)
+            {
+                CartesianProductionTemp = CartesianProductionTemp*AllSets[l];
+            }
+            Console.WriteLine(" The cartesian product of the sets is : ");
+            var cartesian = CartesianProductionTemp.ElementsOfTheSet;
+            for(int i=0;i< cartesian.Count; i += 2)
+            {
+                Console.Write("("+cartesian[i]+";");
+                Console.WriteLine(cartesian[i + 1]+")");
+                
+            }
+          
+            Console.ReadKey();
+        }
+        
+    }
+    
+}
---
+++ CartesianProduct2/Sets.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartesianProduct2
+{
+    public class Sets
+    {
+        List<int> elementsOfTheSet;
+        public List<int> ElementsOfTheSet
+        {
+            get
+            {
+                return elementsOfTheSet;
+            }
+            set
+            {
+                elementsOfTheSet = value;
+            }
+        }
+        //Constructor with params
+        public Sets(List<int> production)
+        {
+            elementsOfTheSet = production;
+        }
+        //Constructor with no params
+        public Sets()
+        {
+            elementsOfTheSet = new List<int>();
+        }
+
+        //1 - Mbushja e 1 bashkesie me elemente dhe ruajtja e tyre ne 1 property qe me pas do te therritet
+
+        public List<int> SetElementsToTheSet(int TotalNumbers)
+        {
+            List<int> ElementsOfSet = new List<int>();
+            for (int i = 0; i < TotalNumbers; i++)
+            {
+                Console.WriteLine(" Give element to the set ");
+                string element = Console.ReadLine();
+                int elementPut = Convert.ToInt32(element);
+                ElementsOfSet.Add(elementPut);
+            }
+            elementsOfTheSet = ElementsOfSet; 
+            return elementsOfTheSet;
+        }
+
+
+
+        public static Sets operator *(Sets first, Sets second)
+        {
+            Sets set = new Sets();
+            foreach (int firstInt in first.ElementsOfTheSet)
+            {
+                foreach (int secondInt in second.ElementsOfTheSet)
+                {
+
+                    set.ElementsOfTheSet.Add(firstInt);
+                    set.ElementsOfTheSet.Add(secondInt);
+
+                }
+            }
+            return set;
+        }
+        
+    }
+}
